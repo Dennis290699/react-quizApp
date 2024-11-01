c3432bffa62dbed57f94f04b88b1e4fdf92566b5
Dennis290699 <trujilloadrian29@gmail.com>
2024-11-01 14:48:28 -0500

    Update categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,5 +1,9 @@
-import { networkOSQuestions } from './networkOSQuestions';
+import { chapter1Questions } from './chapter1Questions';
+import { chapter2Questions } from './chapter2Questions';
+import { chapter3Questions } from './chapter3Questions';
 
 export const categories = [
-  networkOSQuestions,
+  chapter1Questions,
+  chapter2Questions,
+  chapter3Questions,
 ];
