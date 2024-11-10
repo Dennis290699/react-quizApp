8c288ca147eb4b22f18c1cc3daeb33117250e03e
Dennis290699 <trujilloadrian29@gmail.com>
2024-11-10 15:26:15 -0500

    Create chapter4Questions.ts

---
+++ src/data/chapter4Questions.ts
@@ -0,0 +1,12 @@
+import { QuizCategory } from '../types/quiz';
+
+export const chapter4Questions: QuizCategory = {
+  id: 'network-rigor',
+  title: 'Chapter 4',
+  description: 'Evalúa tus conocimientos sobre los conceptos básicos de redes.',
+  icon: 'Orbit',
+  color: 'indigo',
+  questions: [
+    
+  ]
+};
