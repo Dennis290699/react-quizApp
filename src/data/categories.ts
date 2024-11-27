2fa860a30037de26fe4c218f5219a63e9cd75dd7
Dennis290699 <trujilloadrian29@gmail.com>
2024-11-26 21:45:42 -0500

    Update categories.ts
    Update for questions IA

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,13 +1,5 @@
-import { chapter1Questions } from './chapter1Questions';
-import { chapter2Questions } from './chapter2Questions';
-import { chapter3Questions } from './chapter3Questions';
-import { chapter4Questions } from './chapter4Questions';
-import { chapter5Questions } from './chapter5Questions';
+import { chapterIA } from './chapterIA';
 
 export const categories = [
-  chapter1Questions,
-  chapter2Questions,
-  chapter3Questions,
-  chapter4Questions,
-  chapter5Questions,
+  chapterIA
 ];
